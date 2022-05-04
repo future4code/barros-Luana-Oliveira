2b081d3e67d8a3882917305aeee0b26c5bf7624d
Luana Barbosa de Oliveira <deoliveiraluana29@gmail.com>
2022-05-03 22:08:14 -0300

    lacos

---
+++ modulo1/projeto-blackjack/Blackjack/desafio.js
@@ -0,0 +1,22 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+let boasVindas = `Boas vindas ao jogo de Blackjack!`
+   console.log(boasVindas);
+
+   (confirm("Quer iniciar uma nova rodada?"))
+   if(ok){
+      console.log(`Iniciar nova rodada`);
+
+   }
+
+

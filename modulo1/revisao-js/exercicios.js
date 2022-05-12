3aaf0266f0df02c8bc95b1a462e13e292ef1c611
Luana Barbosa de Oliveira <deoliveiraluana29@gmail.com>
2022-05-12 20:11:53 -0300

    Exercicios 01 a 06

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -6,31 +6,58 @@
 // EXERCÍCIO 01 // Escreva uma função que recebe um array como parâmetro e retorna a quantidade de elementos que há nele.
 function retornaTamanhoArray(array) {
    
+    return array.length
+
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  
+    
+    return array.reverse()
+
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+
+    function arrayOrdernado( x,y){
+        return (x-y)
+    }
+  return array.sort(arrayOrdernado)   
 }
+
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
   
+    return array.filter((array) => array % 2 == 0)
+
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
  
+    const novoArray = []
+    array.filter((numero)=>{
+        if(numero % 2 == 0){
+        novoArray.push(numero ** 2)    
+        }
+
+    })
+    return novoArray
 }
 
-// EXERCÍCIO 06
+// EXERCÍCIO 06 // Escreva uma função que receba um array de números e retorne o maior número dele.
 function retornaMaiorNumero(array) {
   
+    maiorNumero = array[0]
+    for (numero of array){
+        if (numero > maiorNumero){
+            maiorNumero = numero
+        }
+
+    }
+    return maiorNumero    
 }
 
 // EXERCÍCIO 07

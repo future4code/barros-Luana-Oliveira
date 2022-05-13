8876131eefcbb76b2dc2eeaf0289d222fea9c3d2
Luana Barbosa de Oliveira <deoliveiraluana29@gmail.com>
2022-05-12 21:20:03 -0300

    Exercicios 01 a 13A

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -63,36 +63,91 @@ function retornaMaiorNumero(array) {
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
 
+    let maior
+    let menor
+        if (num1 > num2){
+            maior = num1
+            menor = num2
+        } else {
+            maior = num2
+            menor = num1
+        }
+    return {
+        maiorNumero: maior,
+        maiorDivisivelPorMenor: maior % menor == 0 ? true:false,
+        diferenca: maior - menor
+    }
 }
 
-// EXERCÍCIO 08
+// EXERCÍCIO 08 // Escreva uma função que, recebendo um número N como parâmetro, retorne os N primeiros números pares
 function retornaNPrimeirosPares(n) {
    
+    let numeros = [0]
+        i = 0
+        while(numeros.length < n){
+            i+=2
+            numeros.push(i)
+        }
+        return numeros
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
 
+    if (ladoA !== ladoB && ladoB !== ladoC && ladoC !== ladoA){
+        return "Escaleno"
+    } else if (ladoA == ladoB && ladoA == ladoC){
+    return "Equilátero"
+    } else {
+        return "Isósceles"
+    }
 }
+
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
   
+    function maiorMenor(x,y){
+        return (x-y)
+    }
+array.sort(maiorMenor) 
+segundoMaior = array[array.length-2]
+segundoMenor = array[1]
+return [segundoMaior, segundoMenor]
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
    
+    let frase = ""
+    for (let ator of filme.atores) {
+        frase += ator + ", "
+    }
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
 }
+
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
    
+    let pessoaAnonima = {
+        ...pessoa,
+        nome: "ANÔNIMO"
+
+    }
+    return pessoaAnonima
+
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
    
+    pessoasAutorizadas = pessoas.filter((item)=>{
+        return item.idade > 14 && item.idade < 60 && item.altura >= 1.5
+    })
+ return pessoasAutorizadas
+
 }
 
 // EXERCÍCIO 13B
